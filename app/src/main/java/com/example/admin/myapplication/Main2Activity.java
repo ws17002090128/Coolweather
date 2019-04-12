eb457c300503a3a36668226f8b88f6e7e644f141
wasdf <adg@qq.com>
2019-04-12 09:24:11 +0800

    123

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -7,8 +7,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -17,6 +23,8 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
+    private String[] data={"北京","浙江","安徽"};
+    private ListView listView;
     private TextView textView;
     private Button button;
     @Override
@@ -25,6 +33,9 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         this.textView=(TextView)findViewById(R.id.main2);
         this.button=(Button)findViewById(R.id.button2);
+        this.listView=(ListView)findViewById(R.id.listview);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(adapter);
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +53,7 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText=response.body().string();
+                String[] result=parseJSONObject(responseText);
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -49,6 +61,22 @@ public class Main2Activity extends AppCompatActivity {
                     }
                 });
 
+            }
+
+            private String[] parseJSONObject(String responseText) {
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = new JSONArray(responseText);
+                    String[] result = new String[jsonArray.length()];
+                    for (int i = 0; i<jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        result[i] = jsonObject.getString("name");
+                    }
+                    return result;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
             }
         });
     }
